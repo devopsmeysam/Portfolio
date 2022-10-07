10b93fdd5463e4f8ef5ed0fed114932921a9f896
devopsmeysam <91230909+devopsmeysam@users.noreply.github.com>
2022-10-07 14:51:23 -0400

    Defining Routes for all pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,43 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home Page', name: 'Meysam' });
+});
+
+/* GET About me page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', 
+  { 
+    title: 'About Me',
+    name: 'Meysam'
+  });
+});
+
+/* GET Contact me page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', 
+  { 
+    title: 'Contact Me',
+    name: 'Meysam'
+  });
+});
+
+/* GET My Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', 
+  { 
+    title: 'My Projects',
+    name: 'Meysam'
+  });
+});
+
+/* GET My Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', 
+  { 
+    title: 'My Services',
+    name: 'Meysam'
+  });
 });
 
 module.exports = router;
